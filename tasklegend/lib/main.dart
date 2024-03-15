b5236f6dd4a6b54f080f925dc39ae78c2ff67597
sebanepe <144700492+sebanepe@users.noreply.github.com>
2024-03-15 11:48:37 -0400

    User Manual
    Se añadio el manual de usuario al boton ayuda ademas de quitar el boton de reporte de tareas pues era redundante dada la funcionalidad dentro de lista de tareas

--- tasklegend/lib/main.dart
+++ tasklegend/lib/main.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:tasklegend/confscreen.dart';
 import 'package:tasklegend/ventanas/lista_tareas.dart';
 
+String manualDeUsuario = """
+¡Bienvenido a Task Legends!
+Esta app permitirá a verdaderos guerreros Z administrar su tiempo para ser la mejor versión de sí mismos.
+Solo debes presionar en una de las dos opciones, "Lista de tareas" o configuración si deseas realizar algunos ajustes estéticos.
+En "Lista de tareas" se maneja completamente la app, permitiendo añadir, quitar o actualizar tareas.  
+""";
+
 void main() {
   runApp(MyApp());
 }
@@ -15,10 +22,8 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
         // Personalizando colores del AppBar y los iconos aquí
         appBarTheme: AppBarTheme(
-          
           color: Colors.black, // Color de fondo del AppBar
           iconTheme: IconThemeData(color: Colors.orange), // Color de los iconos
-          
         ),
       ),
       home: MyHomePage(),
@@ -36,72 +41,77 @@ class MyHomePage extends StatelessWidget {
             icon: Icon(Icons.settings),
             onPressed: () {
               Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => ConfiguracionesScreen()),
+                context,
+                MaterialPageRoute(builder: (context) => ConfiguracionesScreen()),
               );
               // Acción para la configuración
             },
-            
           ),
           //espacio entre iconos
           SizedBox(width: 12),
           IconButton(
             icon: Icon(Icons.help_outline),
             onPressed: () {
-              // Acción para ayuda
+              // Muestra el AlertDialog con el manual de usuario
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Manual de Usuario'),
+                    content: Text(manualDeUsuario),
+                    backgroundColor: Colors.orange, // Color de fondo del AlertDialog
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cerrar'),
+                      ),
+                    ],
+                  );
+                },
+              );
             },
           ),
         ],
       ),
       body: Container(
-        // Color de fondo que aparecerá donde la imagen no llegue a cubrir
         color: Color(0xFF003366),
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
               image: AssetImage("assets/images/fondolegends.jpg"),
-              fit: BoxFit.contain, // Cambia a BoxFit.cover si deseas que la imagen cubra todo, pero se recorte
+              fit: BoxFit.contain,
             ),
           ),
           child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 200.0, // Establece un ancho para tu logo si es necesario
-                child: Image.asset('assets/images/tasklegends.jpg', fit: BoxFit.contain),
-              ),
-              SizedBox(height: 48), // Espacio entre el logo y los botones
-              ElevatedButton(
-                onPressed: () {
-                  // Acción para la Lista de Tarea
-                  Navigator.push(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 200.0, // Establece un ancho para tu logo si es necesario
+                  child: Image.asset('assets/images/tasklegends.jpg', fit: BoxFit.contain),
+                ),
+                SizedBox(height: 48), // Espacio entre el logo y el botón
+                ElevatedButton(
+                  onPressed: () {
+                    // Acción para la Lista de Tarea
+                    Navigator.push(
                       context,
-                  MaterialPageRoute(builder: (context) => TaskListScreen()),);
-                },
-                child: Text('Lista de Tareas'),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black, backgroundColor: Colors.orange,
-                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                      MaterialPageRoute(builder: (context) => TaskListScreen()),
+                    );
+                  },
+                  child: Text('Lista de Tareas'),
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.black, backgroundColor: Colors.orange,
+                    padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+                  ),
                 ),
-              ),
-              SizedBox(height: 24), // Espacio entre botones
-              ElevatedButton(
-                onPressed: () {
-                  // Acción para el Reporte de Tareas
-                },
-                child: Text('Reporte de Tareas'),
-                style: ElevatedButton.styleFrom(
-                  foregroundColor: Colors.black, backgroundColor: Colors.orange,
-                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
-      ),
     );
-
   }
 }
